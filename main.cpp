381600feb1e9f6eaea66532704d0ad062fb591dc
traagel <marttraagel@gmail.com>
2021-12-25 08:47:20 +0200

    added drift calculations
    bug: initial velocities at impossible values

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
-//#include <vector>
+#include <vector>
 //#include <algorithm>
 #include <cmath>
 
+// Write an action using cout. DON'T FORGET THE "<< endl"
+// To debug: cerr << "Debug messages..." << endl;
 
 double euclidean_distance(int x, int y, int next_checkpoint_x, int next_checkpoint_y){
     double dist;
@@ -21,9 +23,11 @@ bool angle_cone(int next_checkpoint_angle, int angle){
     }
 }
 
-float get_speed(int x, int y, int last_x, int last_y){
-    float spd = (float(x) - float(last_x)) / (float(y) - float(last_y));
-    return spd;
+std::vector<int> get_x_y_velocity(int x, int y, int last_x, int last_y){
+    int vel_x = x - last_x;
+    int vel_y = y - last_y;
+    std::vector <int> speed_vector = {vel_x, vel_y};
+    return speed_vector;
 }
 
 float get_acceleration(float spd, float spd_old){
@@ -48,8 +52,13 @@ int main()
 
     // derived stuff
     int boosts_left = 1;
-    float dist;
-    float spd;
+    double checkpoint_distance;
+    double velocity;
+
+    int k; // drift adjustment constant k
+    std::vector<int> vel_xy_vect;
+    int target_x; // recalculated x value
+    int target_y; // recalculated y value
 
     // game loop
 #pragma clang diagnostic push
@@ -66,27 +75,46 @@ int main()
         std::cin >> opponent_x >> opponent_y;
         std::cin.ignore();
 
-        dist = euclidean_distance(x, y, next_checkpoint_x, next_checkpoint_y);
-        spd = euclidean_distance(x,y,last_x,last_y);
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
+        checkpoint_distance = euclidean_distance(x, y, next_checkpoint_x, next_checkpoint_y);
+        velocity = euclidean_distance(x,y,last_x,last_y);
+
+        // calculate thrust
         if (angle_cone(next_checkpoint_angle, 90)){
             thrust = "15";
-        } else if (boosts_left > 0 and dist > 7000 and not angle_cone(next_checkpoint_angle, 15)){
-            std::cerr << "distance: " << dist << ", using boost" << std::endl;
+        } else if (boosts_left > 0 and checkpoint_distance > 7000 and not angle_cone(next_checkpoint_angle, 15)){
+            std::cerr << "distance: " << checkpoint_distance << ", using boost" << std::endl;
             thrust = "BOOST";
             boosts_left = 0;
-        } else if(dist > 2500) {
+        } else if(checkpoint_distance > 2500) {
             thrust = "100";
         } else {
             thrust = "45";
         }
 
+        //adjust for drift, some constant * x / y speed in opposite direction
+        vel_xy_vect = get_x_y_velocity(x, y, last_x, last_y);
+        k = 2; //starting from 2
+        target_x = next_checkpoint_x; //- k*vel_xy_vect[0];
+        target_y = next_checkpoint_y; //- k*vel_xy_vect[1];
+
         // You have to output the target position
         // followed by the power (0 <= thrust <= 100)
         // i.e.: "x y thrust"
-        std::cerr << "dist: " << dist << "speed: " << spd <<  std::endl;
-        std::cout << next_checkpoint_x << " " << next_checkpoint_y << " " << thrust << std::endl;
+        std::cerr << "calc dist: " << checkpoint_distance << std::endl;
+        std::cerr << "input dist: " << next_checkpoint_dist << std::endl;
+        std::cerr << "speed: " << velocity <<  std::endl;
+        std::cerr << "checkpoint x: " << next_checkpoint_x << ", y:" << next_checkpoint_y << std::endl;
+        std::cerr << "adjusted x: " << target_x << ", y:" << target_y << std::endl;
+        // x, y, last_x, last_y
+        std::cerr << "x:" << x << std::endl;
+        std::cerr << "y:" << y << std::endl;
+        std::cerr << "last_x:" << last_x << std::endl;
+        std::cerr << "last_y:" << last_y << std::endl;
+
+        //
+        std::cerr << "vel x: " << vel_xy_vect[0] << std::endl;
+        std::cerr << "vel y: " << vel_xy_vect[1] << std::endl;
+        std::cout << target_x << " " << target_y << " " << thrust << std::endl;
 
     }
 #pragma clang diagnostic pop
