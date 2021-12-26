055a77855ce513255b37d4a8d89188cb9971cf7c
traagel <marttraagel@gmail.com>
2021-12-26 08:19:06 +0200

    refactoring everything ! 1) storing pod info as vectors 2) new information provided so some old functions are redundant 3) reading input works ! 4) output works !

--- main.cpp
+++ main.cpp
@@ -4,6 +4,16 @@
 //#include <algorithm>
 #include <cmath>
 
+int calculate_boost(int angle, int range){ // return range from 0 to 100 depending on relative angle, if at 90 degrees, boost = 0
+    int boost = 0;
+    if (angle > 90){
+        return boost;
+    } else {
+        boost = (1 - angle/90)*100;
+    }
+    return boost;
+}
+
 int main() {
     // Initialization input
     // Line 1: laps : the number of laps to complete the race.
@@ -13,25 +23,17 @@ int main() {
     int checkpoint_count;
     int checkpoint_id_one;
     int checkpoint_id_two;
-    // coordinates of the checkpoints
-    std::vector<std::vector<int>> checkpoints;
+    int boost_one;
+    int boost_two;
+    int k;
+    std::vector<std::vector<int>> checkpoints; // coordinates of the checkpoints
 
-    /*
-     * Input for one game turn
-     * First 2 lines: Your two pods.
-     * Next 2 lines: The opponent's pods.
-     * Each pod is represented by: 6 integers,
-         * x & y for the position.
-         * vx & vy for the speed vector.
-         * angle for the rotation angle in degrees.
-         * nextCheckPointId for the number of the next checkpoint the pod must go through.
-    */
     // Declaring my pods my_pod_one, my_pod_two, enemy pods as: enemy_pod_one, enemy_pod_two;
+    // indices & variables - 0 x, 1 y, 2 vx, 3 vy, 4 angle, 5 next checkpoint id
     std::vector<int> my_pod_one;
     std::vector<int> my_pod_two;
     std::vector<int> enemy_pod_one;
     std::vector<int> enemy_pod_two;
-    // Vector of pods
     std::vector<std::vector<int>> pods = {my_pod_one, my_pod_two, enemy_pod_one, enemy_pod_two};
 
     // temp variables
@@ -44,18 +46,19 @@ int main() {
     i = 0;
     std::cerr << "laps: " << laps << ", checkpoint_count: " << checkpoint_count << std::endl;
 
+    // initialize the checkpoint coordinates vector
     while (i < checkpoint_count){
         for (int j = 0; j < 2; j++){
             std::cin >> cin_input;
             coordinates.push_back(cin_input);
-            std::cerr << "read coordinate: " << cin_input << std::endl;
         }
         std::cin.ignore();
         checkpoints.push_back(coordinates);
         coordinates.clear();
         i++;
     }
-    // initialize pod vectors
+
+    // initialize pod vectors - default values 0 so i can insert by index later
     for (std::vector<int>& pod: pods){
         i = 0;
         while (i < 6) {
@@ -67,13 +70,13 @@ int main() {
     // Game loop
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
-    while (1) {
+    while (true) {
         // updating pod vectors - 6 input integers
         for (std::vector<int>& pod: pods) { // loop through every pod in pods
             i = 0;
-            while (i < 6) { // i goes 0 to 5 because 6 integer variables get pushed into every vector
+            while (i < 6) { // 6 integer variables per pod vector
                 std::cin >> cin_input;
-                pod[i] = cin_input; // something wrong with this line
+                pod[i] = cin_input; //
                 i++;
             }
             std::cin.ignore();
@@ -82,10 +85,15 @@ int main() {
         // outputs
         checkpoint_id_one = pods[0][5];
         checkpoint_id_two = pods[1][5];
+        boost_one = calculate_boost(pods[0][4], 0);
+        boost_two = calculate_boost(pods[1][4], 0);
+        k = 2;
+
         // my pod 1
-        std::cout << checkpoints[checkpoint_id_one][0] << " " << checkpoints[checkpoint_id_one][1] << " 100" << std::endl;
+        std::cout << checkpoints[checkpoint_id_one][0] - k*pods[0][2] << " " <<
+                     checkpoints[checkpoint_id_one][1] - k*pods[0][3] << " " << boost_one << std::endl;
         // my pod 2
-        std::cout << checkpoints[checkpoint_id_two][0] << " " << checkpoints[checkpoint_id_two][1] << " 100" << std::endl;
+        std::cout << checkpoints[checkpoint_id_two][0] << " " << checkpoints[checkpoint_id_two][1] << " " << boost_two << std::endl;
 
     }
 // #pragma clang diagnostic pop
